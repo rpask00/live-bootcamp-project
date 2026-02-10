12d22a1f2d9204b4db498d93b1946c27ab7ff1a2
Rafal Pasko <rp.261000@gmail.com>
2026-02-10 23:45:11 +0100

    [Sprint 3][Task 2] fix: fix docker.sh

--- docker.sh
+++ docker.sh
@@ -22,5 +22,5 @@ while IFS= read -r line; do
 done < <(grep -v '^#' "$ENV_FILE")
 
 # Run docker-compose commands with exported variables
-docker-compose build
-docker-compose up
+docker compose build
+docker compose up
