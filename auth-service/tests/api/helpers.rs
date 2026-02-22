dda71f4566f1c7c366489dc08ee5a54bcdcd3e46
Rafal Pasko <rp.261000@gmail.com>
2026-02-22 22:22:59 +0100

    [Sprint 5][Task 3] fix: panic on TestApp drop if database is not cleaned up.

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -7,8 +7,9 @@ use auth_service::utils::constants::test;
 use auth_service::{get_postgres_pool, Application};
 use dotenv::dotenv;
 use reqwest::cookie::Jar;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::{Executor, PgPool};
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 use uuid::Uuid;
@@ -20,11 +21,13 @@ pub struct TestApp {
     pub banned_token_store: BannedTokenStoreType,
     pub two_fa_code_store: TwoFACodeStoreType,
     pub email_client: EmailClientType,
+    pub db_name: String,
+    cleaned_up: bool,
 }
 
 impl TestApp {
     pub async fn new() -> Self {
-        let pg_pool = configure_postgresql().await;
+        let (pg_pool, db_name) = configure_postgresql().await;
         // let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
         let user_store = Arc::new(RwLock::new(PostgresUserStore::new(pg_pool)));
 
@@ -61,6 +64,8 @@ impl TestApp {
             banned_token_store: banned_token_store.clone(),
             two_fa_code_store: two_fa_code_store.clone(),
             email_client: email_client.clone(),
+            db_name,
+            cleaned_up: false,
         }
     }
 
@@ -131,26 +136,42 @@ impl TestApp {
     pub fn get_random_email() -> String {
         format!("{}@example.com", Uuid::new_v4())
     }
+
+    pub async fn clean_up(&mut self) {
+        self.cleaned_up = true;
+        delete_database(self.db_name.as_str()).await;
+    }
 }
 
-async fn configure_postgresql() -> PgPool {
+impl Drop for TestApp {
+    fn drop(&mut self) {
+        if !self.cleaned_up {
+            panic!("TestApp has not been cleaned up!")
+        }
+    }
+}
+
+async fn configure_postgresql() -> (PgPool, String) {
     dotenv().ok();
 
-    let postgresql_conn_url = std::env::var("DATABASE_URL")
+    let database_url = std::env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set")
         .to_owned();
 
     // We are creating a new database for each test case, and we need to ensure each database has a unique name!
     let db_name = Uuid::new_v4().to_string();
 
-    configure_database(&postgresql_conn_url, &db_name).await;
+    configure_database(&database_url, &db_name).await;
 
-    let postgresql_conn_url_with_db = format!("{}/{}", postgresql_conn_url, db_name);
+    let postgresql_conn_url_with_db = format!("{}/{}", database_url, db_name);
 
     // Create a new connection pool and return it
-    get_postgres_pool(&postgresql_conn_url_with_db)
-        .await
-        .expect("Failed to create Postgres connection pool!")
+    (
+        get_postgres_pool(&postgresql_conn_url_with_db)
+            .await
+            .expect("Failed to create Postgres connection pool!"),
+        db_name,
+    )
 }
 
 async fn configure_database(db_conn_string: &str, db_name: &str) {
@@ -179,4 +200,42 @@ async fn configure_database(db_conn_string: &str, db_name: &str) {
         .run(&connection)
         .await
         .expect("Failed to migrate the database");
+}
+
+async fn delete_database(db_name: &str) {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set")
+        .to_owned();
+
+    let connection_options =
+        PgConnectOptions::from_str(&database_url).expect("Failed to parse PostgreSQL connection string");
+
+    let mut connection = PgConnection::connect_with(&connection_options)
+        .await
+        .expect("Failed to connect to Postgres");
+
+    // Kill any active connections to the database
+    connection
+        .execute(
+            format!(
+                r#"
+                SELECT pg_terminate_backend(pg_stat_activity.pid)
+                FROM pg_stat_activity
+                WHERE pg_stat_activity.datname = '{}'
+                  AND pid <> pg_backend_pid();
+        "#,
+                db_name
+            )
+            .as_str(),
+        )
+        .await
+        .expect("Failed to drop the database.");
+
+    // Drop the database
+    connection
+        .execute(format!(r#"DROP DATABASE "{}";"#, db_name).as_str())
+        .await
+        .expect("Failed to drop the database.");
 }
--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -5,7 +5,7 @@ use serde_json::json;
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
-    let app = TestApp::new().await;
+    let mut app = TestApp::new().await;
 
     let response = app
         .post_verify_token(&json!({
@@ -14,11 +14,13 @@ async fn should_return_422_if_malformed_input() {
         .await;
 
     assert_eq!(response.status().as_u16(), 422);
+
+    app.clean_up().await;
 }
 
 #[tokio::test]
 async fn should_return_200_valid_token() {
-    let app = TestApp::new().await;
+    let mut app = TestApp::new().await;
 
     let jwt = generate_auth_cookie(&Email::parse(TestApp::get_random_email()).unwrap())
         .expect("Failed to generate auth cookie");
@@ -30,11 +32,12 @@ async fn should_return_200_valid_token() {
         .await;
 
     assert_eq!(response.status().as_u16(), 200);
+    app.clean_up().await;
 }
 
 #[tokio::test]
 async fn should_return_401_if_invalid_token() {
-    let app = TestApp::new().await;
+    let mut app = TestApp::new().await;
 
     let response = app
         .post_verify_token(&json!({
@@ -43,11 +46,12 @@ async fn should_return_401_if_invalid_token() {
         .await;
 
     assert_eq!(response.status().as_u16(), 401);
+    app.clean_up().await;
 }
 
 #[tokio::test]
 async fn should_return_401_if_banned_token() {
-    let app = TestApp::new().await;
+    let mut app = TestApp::new().await;
 
     let jwt = generate_auth_cookie(&Email::parse(TestApp::get_random_email()).unwrap())
         .expect("Failed to generate auth cookie");
@@ -71,4 +75,5 @@ async fn should_return_401_if_banned_token() {
         .await;
 
     assert_eq!(response.status().as_u16(), 401);
+    app.clean_up().await;
 }
